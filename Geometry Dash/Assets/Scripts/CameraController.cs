7bcfc42df1a54e4c5a472d4bb9f9481e12ad3546
megumin <meegumin@users.noreply.github.com>
2019-12-04 18:37:32 +0300

    Get started!

---
+++ Geometry Dash/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offset;
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
---
+++ Geometry Dash/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private float staticSpeed = 60f;
+    [SerializeField]
+    private float jumpForce = 20f;
+    [SerializeField]
+    private bool isGround = false;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += Vector3.right * staticSpeed;
+        if(Input.GetKey(KeyCode.Space) && isGround)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = false;
+        }
+    }
+}
