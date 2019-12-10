95e7abec2696431d79264238fd08881083460a00
megumin <meegumin@users.noreply.github.com>
2019-12-10 16:37:49 +0300

    Correct

--- Geometry Dash/Assets/Scripts/PlayerMovement.cs
+++ Geometry Dash/Assets/Scripts/PlayerMovement.cs
@@ -17,12 +17,28 @@ public class PlayerMovement : MonoBehaviour
     private TextMeshProUGUI score;
     private bool isGround = false;
 
+    [SerializeField]
+    private float rotationSpeed = 3f;
+    private Vector3 rotation = Vector3.zero;
     void FixedUpdate()
     {
         rb.AddForce(Vector3.right * staticSpeed);
         if(Input.GetKey(KeyCode.Space) && isGround)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            int dir = Random.Range(1, 3);
+            if(dir == 1)
+            {
+                rotation = Vector3.up;
+            }
+            else
+            {
+                rotation = Vector3.up * -1;
+            }
+        }
+        if(!isGround)
+        {
+            transform.Rotate(rotation, rotationSpeed);
         }
         speed.text = "SPEED " + Mathf.Round(rb.velocity.x);
         score.text = "SCORE " + Mathf.Round(transform.position.x);
