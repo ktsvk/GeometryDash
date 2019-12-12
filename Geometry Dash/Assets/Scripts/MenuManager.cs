4b7ad1643c0c63e7bfad9c1c8a15d56ede337d0c
Ruskssss <sukharev2000@list.ru>
2019-12-12 13:08:20 +0300

    Main_Menu

---
+++ Geometry Dash/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
+    public void ExitButton()
+    { 
+        Application.Quit();  
+    }
+
+    public void ChangeLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
